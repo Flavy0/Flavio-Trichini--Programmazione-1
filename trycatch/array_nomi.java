649cc6228a2bb7a868b2eca9b2169aeb058881dd
Flavy0 <160338570+Flavy0@users.noreply.github.com>
2025-06-27 11:27:33 +0200

    Update array_nomi.java

--- trycatch/array_nomi.java
+++ trycatch/array_nomi.java
@@ -6,17 +6,15 @@ public class array_nomi {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-        System.out.println("Inserisci numero:");
-        String ns = scanner.nextLine();
-        scanner.close();
-        int n = Integer.valueOf(ns);
-        String[] nomi= {"giulio", "ugo", "anna", "gabriele", "fernando"};
-        try {
-        	System.out.println(nomi[n]);
-        } catch (Exception e) {
-        	System.out.println("Errore");
-        } finally {
-        	ns.close();
-        }
+	        System.out.println("Inserisci numero:");
+	        String ns = scanner.nextLine();
+	        scanner.close();
+	        int n = Integer.valueOf(ns);
+	        String[] nomi= {"giulio", "ugo", "anna", "gabriele", "fernando"};
+	        try {
+	        	System.out.println(nomi[n]);
+	        } catch (Exception e) {
+	        	System.out.println("Errore");
+	        }
 	}
 }
