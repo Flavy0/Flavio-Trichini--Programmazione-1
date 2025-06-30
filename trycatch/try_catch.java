56ecc858054f61cbb3c9359faf594894cdd5612e
Flavy0 <160338570+Flavy0@users.noreply.github.com>
2025-06-30 12:39:15 +0200

    Update try_catch.java

--- trycatch/try_catch.java
+++ trycatch/try_catch.java
@@ -1,43 +1,14 @@
-package sj_30_06;
+package sj_27_06;
 
-import java.util.Stack;
+public class try_catch {
 
-public class Browser {
-    private Stack<String> history;
-
-    public Browser() {
-        history = new Stack<>();
-        System.out.println("Browser avviato. Cronologia vuota");
-    }
-
-    public void visitPage(String url) {
-        history.push(url);
-        System.out.println("Visitata: " + url);
-    }
-
-    public String goBack() {
-        try {
-            String currentPage = history.pop();
-            String previousPage = history.peek();
-            System.out.println("Tornato/a a: " + previousPage + " (era su " + currentPage + ")");
-            return previousPage;
-        } catch (Exception e) {
-            System.out.println("Nessuna pagina precedente.");
-            return null;
-        }
-    }
-
-    public void printHistory() {
-        Stack<String> tempStack = new Stack<>();
-        tempStack.addAll(history);
-
-        try {
-            System.out.println("\n- Cronologia di navigazione (dal più recente al meno recente)");
-            while (!tempStack.isEmpty()) {
-                System.out.println("- " + tempStack.pop());
-            }
-        } catch (Exception e) {
-            System.out.println("La cronologia è vuota.");
-        }
-    }
+	public static void main(String[] args) {
+		int a=2;
+		try {
+			a=a/0;
+		} catch (Exception e) {
+			System.out.println("Errore");
+		}
+		
+	}
 }
